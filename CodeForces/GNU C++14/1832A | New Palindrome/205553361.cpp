ec890f69da3d1716c53dd2e6c11bc5810e33b681
Pankaj <82333383+Pankajn19@users.noreply.github.com>
2023-05-19 05:28:43 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1832A | New Palindrome/205553361.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    string s; cin>>s;
+    for(int i=0;i<s.size()/2-1;i++){
+        if(s[i]!=s[i+1]){
+            cout<<"YES\n";
+            return;
+        } 
+ 
+    }
+    cout<<"NO\n";
+ 
+}
+int main()
+{
+    int t; cin>>t;
+    while(t--) solve();
+    return 0;
+}
