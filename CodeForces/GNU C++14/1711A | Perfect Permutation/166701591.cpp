28cddecac0fbc69d50c1491e2ed193d15e865b62
Pankaj <82333383+Pankajn19@users.noreply.github.com>
2023-05-19 05:29:52 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1711A | Perfect Permutation/166701591.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        for(int i=1;i<n;i++){
+            cout<<i+1<<" "; }
+            cout<<1<<endl;
+    }
+    return 0;
+}
