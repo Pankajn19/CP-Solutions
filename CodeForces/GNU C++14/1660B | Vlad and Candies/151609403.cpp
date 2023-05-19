4aa33a549eff8817581d961e9c5fb194b567fb87
Pankaj <82333383+Pankajn19@users.noreply.github.com>
2023-05-19 05:31:55 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1660B | Vlad and Candies/151609403.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+void merge(int array[], int const left, int const mid, int const right)
+{
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+ 
+    // Create temp arrays
+    auto *leftArray = new int[subArrayOne],
+         *rightArray = new int[subArrayTwo];
+ 
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+ 
+    auto indexOfSubArrayOne = 0,   // Initial index of first sub-array
+        indexOfSubArrayTwo = 0;    // Initial index of second sub-array
+    int indexOfMergedArray = left; // Initial index of merged array
+ 
+    // Merge the temp arrays back into array[left..right]
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo)
+    {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo])
+        {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else
+        {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    while (indexOfSubArrayOne < subArrayOne)
+    {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo)
+    {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+void mergeSort(int array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return; // Returns recursively
+ 
+    auto mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+ 
+// UTILITY FUNCTIONS
+// Function to print an array
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        mergeSort(a, 0, n - 1);
+        int max = a[n - 1];
+        int sec = a[n - 2];
+        if (n == 1 && a[0] > 1)
+        {
+            cout << "NO" << endl;
+        }
+        else if (n == 1 && a[0] == 1)
+        {
+            cout << "YES" << endl;
+        }
+ 
+        else if (max > (sec + 1))
+        {
+ 
+            cout << "NO" << endl;
+        }
+        else
+            cout << "YES" << endl;
+    }
+ 
+    return 0;
+}
