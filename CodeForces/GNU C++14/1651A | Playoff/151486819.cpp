b20834b02dc486b873f188a1de2daf1ce8d700a8
Pankaj <82333383+Pankajn19@users.noreply.github.com>
2023-05-19 05:32:01 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1651A | Playoff/151486819.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int x = pow(2, n);
+        cout << x - 1 << endl;
+    }
+    return 0;
+}
